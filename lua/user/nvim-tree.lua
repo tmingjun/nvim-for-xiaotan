dcc0b617f00054780c5b73abc75e843e5fc695d0
xiaotan <1712095030@qq.com>
2022-06-11 23:34:41 -0700

    格式化lua文件

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -3,119 +3,119 @@
 
 local status_ok, nvim_tree = pcall(require, "nvim-tree")
 if not status_ok then
-  return
+    return
 end
 
 local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
 if not config_status_ok then
-  return
+    return
 end
 
 -- Replaces auto_close
 local tree_cb = nvim_tree_config.nvim_tree_callback
 vim.api.nvim_create_autocmd("BufEnter", {
-  nested = true,
-  callback = function()
-    if #vim.api.nvim_list_wins() == 1 and vim.api.nvim_buf_get_name(0):match("NvimTree_") ~= nil then
-      vim.cmd "quit"
+    nested = true,
+    callback = function()
+        if #vim.api.nvim_list_wins() == 1 and vim.api.nvim_buf_get_name(0):match("NvimTree_") ~= nil then
+            vim.cmd "quit"
+        end
     end
-  end
 })
 
 nvim_tree.setup {
-  disable_netrw = true,
-  hijack_netrw = true,
-  open_on_setup = false,
-  ignore_ft_on_setup = {
-    "startify",
-    "dashboard",
-    "alpha",
-  },
-  open_on_tab = false,
-  hijack_cursor = false,
-  update_cwd = true,
-  diagnostics = {
-    enable = true,
-    icons = {
-      hint = "",
-      info = "",
-      warning = "",
-      error = "",
+    disable_netrw = true,
+    hijack_netrw = true,
+    open_on_setup = false,
+    ignore_ft_on_setup = {
+        "startify",
+        "dashboard",
+        "alpha",
     },
-  },
-  update_focused_file = {
-    enable = true,
+    open_on_tab = false,
+    hijack_cursor = false,
     update_cwd = true,
-    ignore_list = {},
-  },
-  system_open = {
-    cmd = nil,
-    args = {},
-  },
-  filters = {
-    dotfiles = false,
-    custom = {},
-  },
-  git = {
-    enable = true,
-    ignore = true,
-    timeout = 500,
-  },
-  view = {
-    width = 30,
-    height = 30,
-    hide_root_folder = false,
-    side = "left",
-    mappings = {
-      custom_only = false,
-      list = {
-        { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
-        { key = "h", cb = tree_cb "close_node" },
-        { key = "v", cb = tree_cb "vsplit" },
-      },
+    diagnostics = {
+        enable = true,
+        icons = {
+            hint = "",
+            info = "",
+            warning = "",
+            error = "",
+        },
     },
-    number = false,
-    relativenumber = false,
-  },
-  trash = {
-    cmd = "trash",
-    require_confirm = true,
-  },
-  actions = {
-    open_file = {
-      quit_on_open = true,
-      window_picker = {
-            enable = false,
-      },
+    update_focused_file = {
+        enable = true,
+        update_cwd = true,
+        ignore_list = {},
     },
-  },
+    system_open = {
+        cmd = nil,
+        args = {},
+    },
+    filters = {
+        dotfiles = false,
+        custom = {},
+    },
+    git = {
+        enable = true,
+        ignore = true,
+        timeout = 500,
+    },
+    view = {
+        width = 30,
+        height = 30,
+        hide_root_folder = false,
+        side = "left",
+        mappings = {
+            custom_only = false,
+            list = {
+                { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
+                { key = "h", cb = tree_cb "close_node" },
+                { key = "v", cb = tree_cb "vsplit" },
+            },
+        },
+        number = false,
+        relativenumber = false,
+    },
+    trash = {
+        cmd = "trash",
+        require_confirm = true,
+    },
+    actions = {
+        open_file = {
+            quit_on_open = true,
+            window_picker = {
+                enable = false,
+            },
+        },
+    },
     renderer = {
         highlight_git = true,
         highlight_opened_files = "all",
         icons = {
-          glyphs = {
-            default = "",
-            symlink = "",
-            folder = {
-              arrow_closed = "",
-              arrow_open = "",
-              default = "",
-              open = "",
-              empty = "",
-              empty_open = "",
-              symlink = "",
-              symlink_open = "",
+            glyphs = {
+                default = "",
+                symlink = "",
+                folder = {
+                    arrow_closed = "",
+                    arrow_open = "",
+                    default = "",
+                    open = "",
+                    empty = "",
+                    empty_open = "",
+                    symlink = "",
+                    symlink_open = "",
+                },
+                git = {
+                    unstaged = "",
+                    staged = "S",
+                    unmerged = "",
+                    renamed = "➜",
+                    untracked = "U",
+                    deleted = "",
+                    ignored = "◌",
+                },
             },
-            git = {
-              unstaged = "",
-              staged = "S",
-              unmerged = "",
-              renamed = "➜",
-              untracked = "U",
-              deleted = "",
-              ignored = "◌",
-            },
-          },
         }
     }
 
